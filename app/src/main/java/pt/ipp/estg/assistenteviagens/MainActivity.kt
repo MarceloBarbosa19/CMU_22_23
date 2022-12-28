814f6954747e8f0d12245cd025c6d24f75f8a3eb
Marcelo8190357 <8190357@estg.ipp.pt>
2022-12-28 16:24:21 +0000

    Error Correction

--- app/src/main/java/pt/ipp/estg/assistenteviagens/MainActivity.kt
+++ app/src/main/java/pt/ipp/estg/assistenteviagens/MainActivity.kt
@@ -3,35 +3,21 @@ package pt.ipp.estg.assistenteviagens
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.CoroutineScope
-import pt.ipp.estg.assistenteviagens.navigation.HomeScreen
 import pt.ipp.estg.assistenteviagens.navigation.appNavigationScreens.models.NavigationItems
 import pt.ipp.estg.assistenteviagens.navigation.authNavigationScreens.models.AuthNavigationItems
 import pt.ipp.estg.assistenteviagens.navigation.authNavigationScreens.screens.LoginScreen
 import pt.ipp.estg.assistenteviagens.navigation.authNavigationScreens.screens.MainScreen
 import pt.ipp.estg.assistenteviagens.navigation.authNavigationScreens.screens.RegisterScreen
 import pt.ipp.estg.assistenteviagens.ui.theme.AssistenteViagensTheme
-import pt.ipp.estg.assistenteviagens.navigation.utils.topsBars.TopBarLogin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,14 +42,6 @@ fun AuthNavigation() {
     val navController = rememberNavController()
     Scaffold(
         scaffoldState = scaffoldState,
-        topBar = {
-
-
-
-
-            // TopBarLogin(navController = navController)
-            // TopBarRegister(navController = navController)
-        }
     ) {
         AuthNavigationScreens(navController = navController)
     }
--- app/src/main/java/pt/ipp/estg/assistenteviagens/navigation/authNavigationScreens/screens/RegisterScreen.kt
+++ app/src/main/java/pt/ipp/estg/assistenteviagens/navigation/authNavigationScreens/screens/RegisterScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -22,12 +23,14 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import pt.ipp.estg.assistenteviagens.Navigation
 import pt.ipp.estg.assistenteviagens.R
+import pt.ipp.estg.assistenteviagens.navigation.authNavigationScreens.models.AuthNavigationItems
 import pt.ipp.estg.assistenteviagens.room.userDatabase.UserViewModel
 import pt.ipp.estg.assistenteviagens.room.userDatabase.entitys.User
 
@@ -42,9 +45,39 @@ fun RegisterScreen(navController: NavHostController) {
     var showPassword by remember { mutableStateOf(false) }
     val isLogin = false
 
-    Column(
+    Box(
         modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxWidth()
+            .height(56.dp)
+            .background(colorResource(id = R.color.color_background_Drawer)),
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            IconButton(onClick = { navController.navigate(AuthNavigationItems.Home.route) }) {
+                Icon(
+                    Icons.Filled.ArrowBack,
+                    contentDescription = "ArrowBackIcon",
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .size(40.dp)
+                        .padding(start = 5.dp)
+                )
+            }
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = AuthNavigationItems.Register.title,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ) {
         OutlinedTextField(
@@ -125,11 +158,11 @@ fun RegisterScreen(navController: NavHostController) {
             border = BorderStroke(1.dp, Color.Black),
             shape = RoundedCornerShape(10.dp),
             onClick = {
-                if(inputName.isNotEmpty() && inputEmail.isNotEmpty() && inputPass.isNotEmpty()){
+                if (inputName.isNotEmpty() && inputEmail.isNotEmpty() && inputPass.isNotEmpty()) {
                     val intent = Intent(mContext, Navigation::class.java)
                     mContext.startActivity(intent)
                     userViewModel.insertUser(User(inputEmail, inputName, inputPass, isLogin))
-                }else{
+                } else {
                     Toast.makeText(mContext, "The fields can´t by empty", Toast.LENGTH_LONG).show()
                 }
             }) {
